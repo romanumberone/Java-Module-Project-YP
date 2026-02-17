058a0ab7320187f051e5dcb8e36a4f781e84f79f
Roman Tsifra <rtsnumberone@yandex.ru>
2026-02-17 21:55:47 +0300

    Проектная работа №1

---
+++ src/main/java/Auto.java
@@ -0,0 +1,16 @@
+public class Auto {
+    String brand;
+    int speed;
+    Auto(String brand, int speed) {
+        this.brand = brand;
+        this.speed = speed;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+}
